4a1bc801f94ef11ba892347d0a4f1b68abb70ccc
Stepan <pris.stepa@mail.ru>
2023-01-25 21:15:51 +0300

    Ошибки исправил, готово.

--- src/main/java/ru/hogwarts/school/controllers/StudentController.java
+++ src/main/java/ru/hogwarts/school/controllers/StudentController.java
@@ -1,12 +1,24 @@
 package ru.hogwarts.school.controllers;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -14,9 +26,11 @@ import java.util.List;
 public class StudentController {
 
     private final StudentService studentService;
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, AvatarService avatarService) {
         this.studentService = studentService;
+        this.avatarService = avatarService;
     }
 
     @GetMapping
@@ -45,11 +59,43 @@ public class StudentController {
     public Faculty findFaculty (@RequestParam long id) {
         return studentService.findStudentFaculty(id);
     }
+    @GetMapping(value = "/{id}/avatar/preview")
+    public ResponseEntity<byte[]> downloadAvatar (@PathVariable Long id) {
+        Avatar avatar = avatarService.findAvatarByStudentId(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+    @GetMapping(value = "/{id}/avatar/avatar")
+    public void downloadAvatar (@PathVariable Long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.findAvatarByStudentId(id);
+
+        Path path = Path.of(avatar.getFilePath());
+
+        try (InputStream is = Files.newInputStream(path);
+             OutputStream os = response.getOutputStream();) {
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength(Math.toIntExact(avatar.getFileSize()));
+            is.transferTo(os);
+        }
+    }
 
     @PostMapping
     public Student createStudent (@RequestBody Student student) {
         studentService.createStudent(student);
         return student;
+    }
+    @PostMapping(value = "/{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar (@PathVariable Long id, @RequestParam MultipartFile avatarFile) throws IOException {
+        if (avatarFile.getSize() > 1024 * 300) {
+            return ResponseEntity.badRequest().body("File is too big");
+        }
+
+        avatarService.uploadAvatar(id, avatarFile);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping
