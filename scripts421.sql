fb0330e1daf66a54622a075c5aee3802e7c06f16
Stepan <pris.stepa@mail.ru>
2023-02-08 18:45:26 +0300

    Домашка отредактирована

--- scripts421.sql
+++ scripts421.sql
@@ -1,14 +1,32 @@
-CREATE TABLE student
-(
-    id   serial PRIMARY KEY,
-    name VARCHAR(255) PRIMARY KEY,
-    age  INTEGER check (age >= 16) DEFAULT 20,
-    faculty_id bigint REFERENCES faculty(id)
-);
+-- CREATE TABLE student
+-- (
+--     id   serial PRIMARY KEY,
+--     name VARCHAR(255) PRIMARY KEY,
+--     age  INTEGER check (age >= 16) DEFAULT 20,
+--     faculty_id bigint REFERENCES faculty(id)
+-- );
+--
+-- CREATE TABLE faculty
+-- (
+--     id   serial PRIMARY KEY,
+--     name VARCHAR(255) PRIMARY KEY,
+--     color  VARCHAR(255) PRIMARY KEY
+-- );
 
-CREATE TABLE faculty
-(
-    id   serial PRIMARY KEY,
-    name VARCHAR(255) PRIMARY KEY,
-    color  VARCHAR(255) PRIMARY KEY
-);
+
+ALTER TABLE student
+    ADD CONSTRAINT age_constrain CHECK ( age >= 16 );
+
+ALTER TABLE student
+    ALTER COLUMN age SET DEFAULT 20;
+
+ALTER TABLE student
+    ADD PRIMARY KEY (name);
+
+ALTER TABLE faculty
+    ADD PRIMARY KEY (color);
+
+ALTER TABLE faculty
+    ADD PRIMARY KEY (name);
+
+
